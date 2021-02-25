68d13b2fbd542aff556b48039bab6023a33e06bc
Alex <csiszaralex@gmail.com>
2021-02-25 10:00:52 +0100

    :sparkles: Add left and right funtions

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import turtle
 import time
 
+
+def jobbra():
+    fej.right(90)
+
+
+def balra():
+    fej.left(90)
+
+
+# def fel():
+#     fej
+
+
 palya = turtle.Screen()
 palya.setup(width=800, height=600)
 palya.bgcolor("green")
 palya.title("Snake")
 palya.tracer(0)
+palya.listen()
+palya.onkey(jobbra, "Right")
+palya.onkey(balra, "Left")
+# palya.onkey(fel, "Up")
+# palya.onkey(le, "Down")
 
-fej =turtle.Turtle()
+fej = turtle.Turtle()
 fej.shape("triangle")
 fej.penup()
 fej.color("yellow")
@@ -17,4 +35,3 @@ while True:
 
     palya.update()
     time.sleep(0.3)
-
